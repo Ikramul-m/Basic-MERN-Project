93de0a8fcac0def867ac9f0c4e552955568784dc
Ikramul-m <i20727172@gmail.com>
2024-05-04 15:22:52 +0530

    Server

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,14 @@
 const express = require('express')
+
+const app = express()
+
+const PORT = 4000
+const hostName = 'localhost'
+
+app.get('/' , (req,res) => {
+    res.send('Api is Working...')
+})
+
+app.listen(PORT, () => {
+    console.log(`listening to the http://${hostName}:${PORT}`)
+})
