b1bec371d01447b33c8cf15b619dcacfd5679610
Ikramul-m <i20727172@gmail.com>
2024-05-04 15:57:18 +0530

    MongoDB

--- backend/server.js
+++ backend/server.js
@@ -1,14 +1,30 @@
-const express = require('express')
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
 
-const app = express()
 
-const PORT = 4000
-const hostName = 'localhost'
 
-app.get('/' , (req,res) => {
-    res.send('Api is Working...')
-})
+// MongoDB starts
+mongoose
+  .connect("mongodb://127.0.0.1:27017/moudb")
+  .then(() => {
+    console.log("Connected successfully");
+  })
+  .catch((error) => {
+    console.log("error", error);
+  });
+//MongoDB ends
+
+
+
+
+const PORT = 4000;
+const hostName = "localhost";
+
+app.get("/", (req, res) => {
+  res.send("Api is Working...");
+});
 
 app.listen(PORT, () => {
-    console.log(`listening to the http://${hostName}:${PORT}`)
-})
+  console.log(`listening to the http://${hostName}:${PORT}`);
+});
