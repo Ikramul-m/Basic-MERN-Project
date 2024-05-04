d5e66873876c08a795e55cbab81c01036154fc34
Ikramul-m <i20727172@gmail.com>
2024-05-04 16:11:58 +0530

    dotenv

--- backend/server.js
+++ backend/server.js
@@ -1,22 +1,25 @@
 const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
+const dotenv = require("dotenv");
 
-
+dotenv.config();
 
 // MongoDB starts
 mongoose
-  .connect("mongodb://127.0.0.1:27017/moudb")
+  .connect(process.env.URI)
   .then(() => {
     console.log("Connected successfully");
+    app.listen(process.env.PORT || 8000, (err) => {
+      if (err) console.log(err);
+
+      console.log("Running successfully at", process.env.PORT);
+    });
   })
   .catch((error) => {
     console.log("error", error);
   });
 //MongoDB ends
-
-
-
 
 const PORT = 4000;
 const hostName = "localhost";
