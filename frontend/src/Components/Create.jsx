edd0a066062075039dca43e0ae82bbac4c3f6c53
Ikramul-m <i20727172@gmail.com>
2024-05-06 15:47:18 +0530

    .

--- frontend/src/Components/Create.jsx
+++ frontend/src/Components/Create.jsx
@@ -1,21 +1,33 @@
 import React, { useState } from "react";
 
 const Create = () => {
-
   const [name, setName] = useState("");
   const [email, setEmail] = useState("");
   const [age, setAge] = useState(0);
 
-  console.log(name,email,age)
+  console.log(name, email, age);
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const addUser = { name, email, age };
+
+    const response = await fetch("http://localhost:5000/api/user", {
+      method: "POST",
+      body: JSON.stringify(addUser),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+
+    const result = await response.json();
+  };
 
   return (
     <div className="container my-2">
       <h2 className="text-center">Enter the data</h2>
-      <form>
+      <form onSubmit={handleSubmit}>
         <div className="mb-3">
-          <label  className="form-label">
-            Name
-          </label>
+          <label className="form-label">Name</label>
           <input
             type="text"
             className="form-control"
@@ -25,9 +37,7 @@ const Create = () => {
         </div>
 
         <div className="mb-3">
-          <label  className="form-label">
-            Email address
-          </label>
+          <label className="form-label">Email address</label>
           <input
             type="email"
             className="form-control"
@@ -38,9 +48,7 @@ const Create = () => {
         </div>
 
         <div className="mb-3">
-          <label  className="form-label">
-            Age
-          </label>
+          <label className="form-label">Age</label>
           <input
             type="Number"
             className="form-control"
