043139cf27f9a88af04d3f963e01a28c39a4ab04
Ikramul-m <i20727172@gmail.com>
2024-05-06 14:38:33 +0530

    .

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,20 @@
 import "./App.css";
 import Navbar from "./Components/Navbar";
 import { BrowserRouter, Router, Route } from "react-router-dom";
+import Create from "./Components/Create";
+import Read from "./Components/Read";
+import Update from "./Components/Update";
 
 function App() {
   return (
+    // <Navbar />
     <div className="App">
       <BrowserRouter>
-        <Navbar />
+      <Router>
+        <Route exact path="/" element={<Create/>}/>
+        <Route exact path="/all" element={<Read/>}/>
+        <Route exact path="/update" element={<Update/>}/>
+      </Router>
       </BrowserRouter>
     </div>
   );
