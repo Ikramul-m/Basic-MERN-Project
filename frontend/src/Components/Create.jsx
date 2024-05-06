467ae4cf54c003f6873c3035d042aead60490469
Ikramul-m <i20727172@gmail.com>
2024-05-06 15:09:07 +0530

    .

--- frontend/src/Components/Create.jsx
+++ frontend/src/Components/Create.jsx
@@ -1,36 +1,43 @@
-import React from "react";
+import React, { useState } from "react";
 
 const Create = () => {
+
+
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [age, setAge] = useState(0);
+  
+
   return (
-    <div className="container my-2">
-      <h2 className="text-center">Enter the data</h2>
+    <div classNameName="container my-2">
+      <h2 classNameName="text-center">Enter the data</h2>
       <form>
-        <div class="mb-3">
-          <label for="exampleInputPassword1" class="form-label">
+        <div className="mb-3">
+          <label for="exampleInputPassword1" className="form-label">
             Name
           </label>
-          <input type="text" class="form-control" />
+          <input type="text" className="form-control" />
         </div>
 
-        <div class="mb-3">
-          <label for="exampleInputEmail1" class="form-label">
+        <div className="mb-3">
+          <label for="exampleInputEmail1" className="form-label">
             Email address
           </label>
           <input
             type="email"
-            class="form-control"
+            className="form-control"
             aria-describedby="emailHelp"
           />
         </div>
 
-        <div class="mb-3">
-          <label for="exampleInputPassword1" class="form-label">
+        <div className="mb-3">
+          <label for="exampleInputPassword1" className="form-label">
             Age
           </label>
-          <input type="Number" class="form-control" />
+          <input type="Number" className="form-control" />
         </div>
 
-        <button type="submit" class="btn btn-primary">
+        <button type="submit" className="btn btn-primary">
           Submit
         </button>
       </form>
